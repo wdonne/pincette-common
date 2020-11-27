4634ea24763ff331b6d32b3b8440cc5f776e8bd9
Werner Donné <werner.donne@pincette.biz>
2020-11-27 11:36:42 +0100

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module net.pincette.common {
+  requires java.logging;
+  requires java.xml;
+  requires java.json;
+  exports net.pincette.cls;
+  exports net.pincette.function;
+  exports net.pincette.io;
+  exports net.pincette.util;
+  exports net.pincette.xml;
+}
